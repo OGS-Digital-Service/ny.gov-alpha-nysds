33c1d9c1b58806a02b6f7779eb3d4069e164b7db
Jeff Knaack <58082485+knaackbuilt@users.noreply.github.com>
2026-09-04 14:45:09 -0400

    final devbox2 tweaks

--- _includes/_hero/_imageload.js
+++ _includes/_hero/_imageload.js
@@ -2,19 +2,29 @@
     // Array of image objects
     const images = [
         {
-            src: 'https://nygovd8devbox1.prod.acquia-sites.com/sites/default/files/styles/hero_wide_tall/public/2026-09/AlbanyAutumn_01.JPG?h=426803fe&itok=ltW-Ml5p&focal_point_preview_value=57x18',
-            alt: 'A wide-angle view of Albany with fall foliage in the forefront.',
-            caption: 'Fall Albany Foliage Cityscape City'
+        src: 'https://nygovd8devbox2.prod.acquia-sites.com/sites/default/files/styles/hero_wide/public/2026-09/AlbanyAutumn_01.JPG?h=26eb8729&itok=SSy3hT4n&focal_point_preview_value=50x50',
+        alt: 'A wide-angle view of Albany with fall foliage in the forefront.',
+        caption: 'Fall Albany Foliage Cityscape City'
         },
         {
-            src: 'https://nygovd8devbox1.prod.acquia-sites.com/sites/default/files/styles/hero_wide_short/public/2026-09/AlbanyAerials_01.JPG?itok=8y_DddHz&focal_point_preview_value=50x50',
-            alt: 'An aerial view of Albany New York',
-            caption: 'Albany Aerial Cityscape City'
+        src: 'https://nygovd8devbox2.prod.acquia-sites.com/sites/default/files/styles/hero_wide_tall/public/2026-09/AlbanyAerials_01.JPG?h=ac4592dd&itok=dQi9UIXV&focal_point_preview_value=66x46',
+        alt: 'An aerial view of Albany New York',
+        caption: 'Albany Aerial Cityscape City'
         },
         {
-            src: 'https://nygovd8devbox1.prod.acquia-sites.com/sites/default/files/styles/hero_wide_tall/public/2026-09/LI_Montauk_Lighthouse_Montauk_Point_State_Park.jpg?h=c5f5e628&itok=OC4a0euu&focal_point_preview_value=50x50',
-            alt: 'A scenic view of a lighthouse on the coast.',
-            caption: 'Montauk Lighthouse Montauk Point State Park Long Island'
+        src: 'https://nygovd8devbox2.prod.acquia-sites.com/sites/default/files/styles/hero_wide_tall/public/2026-09/AlbanyCapitol.jpg?h=b5f6a62e&itok=9sjEkzrW&focal_point_preview_value=50x50',
+        alt: 'A shot of Albany buildings glowing at dusk',
+        caption: 'Albany Capitol Dusk Downtown New York'
+        },
+            {
+        src: 'https://nygovd8devbox2.prod.acquia-sites.com/sites/default/files/styles/hero_wide_tall/public/2026-09/Long_Island_Spring_Daffodils_Bayard_Cutting.gif?itok=qqdBYUcQ&focal_point_preview_value=50x50',
+        alt: 'Spring daffodils bloom in front of an arboretum.',
+        caption: 'Long Island Spring Daffodils Bayard Cutting'
+        },
+        {
+        src: 'https://nygovd8devbox2.prod.acquia-sites.com/sites/default/files/styles/hero_wide_tall/public/2026-09/LI_Montauk_Lighthouse_Montauk_Point_State_Park.jpg?itok=DuN1qWD5&focal_point_preview_value=50x50',
+        alt: 'A scenic view of a lighthouse on the coast.',
+        caption: 'Montauk Lighthouse Montauk Point State Park Long Island'
         }
         // Add more images as needed
     ];
